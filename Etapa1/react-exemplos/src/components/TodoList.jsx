4492a0ad8c78d92d335457a15da96811f308f489
Rafael <kotsimuna@gmail.com>
2025-03-25 10:05:09 -0300

    Cancel editing

--- Etapa1/react-exemplos/src/components/TodoList.jsx
+++ Etapa1/react-exemplos/src/components/TodoList.jsx
@@ -42,6 +42,12 @@ const TodoList = ({ name }) => {
         setTodo(todos.filter((todo) => todo.id !== id));
     }
 
+    // Cancel editing
+    const cancelEditing = () => {
+        setEditingId(null);
+        setEditingText("");
+    }
+
     return (
         <div style={{ textAlign: "center", marginTop: "50px"}}>
             <h2>A fazer: {name}</h2>
@@ -64,6 +70,7 @@ const TodoList = ({ name }) => {
                                     onChange={(event) => setEditingText(event.target.value)}
                                 />
                                 <button onClick={saveEdit}>Salvar</button>
+                                <a href="#" onClick={cancelEditing}>Cancelar</a>
                             </>
                         ) : (
                             <>
