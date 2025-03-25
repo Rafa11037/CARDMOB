2ce6a85f5cc35652866e2f710c16a4a1d5b6086c
Rafael <kotsimuna@gmail.com>
2025-03-25 09:07:51 -0300

    Add TodoList component

--- Etapa1/react-exemplos/src/App.jsx
+++ Etapa1/react-exemplos/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Counter from './components/Counter';
 import Photo from './components/Photo'
 import Album from './components/album';
+import TodoList from './components/TodoList';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -64,6 +65,7 @@ function App() {
 
   return (
     <>
+      <TodoList name="CARDMOB" />
       <Counter title="Contando..." />
       <Counter initial="100" />
       {/* <article>
