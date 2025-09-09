89d85b6cff9e0d4f7b13b846d8a588b5e628954e
Rafa11037 <kotsimuna@gmail.com>
2025-09-09 10:02:04 -0300

    Inclui exemplo e ícone no tabNavigator

---
+++ Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+import { RootStackParamList, TabParamList } from './types';
+
+// Telas do app - área não logada.
+import HomeScreen from "../screens/HomeScreen";
+// importar depois que implementar: DetailsScreen, SettingsScreen
+import RegisterScreen from "../screens/RegisterScreen";
+import LoginScreen from "../screens/LoginScreen";
+
+const AppStack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator<TabParamList>();
+
+function TabNavigator() {
+    return (
+        <Tab.Navigator
+            screenOptions={({route, navigation}) => ({
+              tabBarIcon: ({ color, focused, size}) => {
+                let iconName;
+                if (route.name === "Home") {
+                  iconName = focused ? "home" : "home";
+                }
+                return <FontAwesome name={iconName} size={size} color={color} />
+              },
+              tabBarActiveTintColor: "red",
+              tabBarInactiveTintColor: "grey",
+              headerShown: false,
+            })}
+          >
+            <Tab.Screen name="Home" component={HomeScreen}/>
+            <Tab.Screen name="Settings" component={HomeScreen} />
+            <Tab.Screen name="Register" component={RegisterScreen} />
+        </Tab.Navigator>
+    );
+}
+
+function StackNavigator() {
+  return (
+    <AppStack.Navigator>
+      <AppStack.Screen
+        name="Tabs"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+      <AppStack.Screen
+        name="Details"
+        component={HomeScreen}
+        options={{ title: 'Detalhes' }}
+      />
+      <AppStack.Screen 
+        name="Login"
+        component={LoginScreen}
+        options={{ title: "Acessar" }}
+      />
+    </AppStack.Navigator>
+  );
+}
+
+export default function AppNavigator() {
+  return (
+    <StackNavigator />
+  );
+};
